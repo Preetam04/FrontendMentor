f90f79c717dfbfbaf34a70ccbdc982e8f944dd52
Preetam Patil <106978006+Preetam04@users.noreply.github.com>
2022-07-11 14:04:07 +0530

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        "Softred": "hsl(7, 99%, 70%)",
+        "yellowcustom": "hsl(51, 100%, 49%)",
+        "Darkdesaturatedcyan": "hsl(167, 40%, 24%)",
+        "Darkblue": "hsl(198, 62%, 26%)",
+        "Darkmoderatecyan": "#90d4c5",
+        "Verydarkdesaturatedblue": "hsl(212, 27%, 19%)",
+        "Verydarkgrayishblue": "hsl(213, 9%, 39%)",
+        "Darkgrayishblue": "hsl(232, 10%, 55%)",
+        "Grayishblue": "hsl(210, 4%, 67%)",
+        "White": "hsl(0, 0%, 100%)",
+        "footertext": "hsl(168, 34%, 41%)",
+      },
+      fontFamily: {
+        "barlow": ["Barlow"],
+        "fraunces": ["Fraunces"],
+      },
+      backgroundImage: {
+        "mobile-header": "url('/images/mobile/image-header.jpg')",
+        "desktop-header": "url(/images/desktop/image-header.jpg)",
+        "cherry-mobile": "url(/images/mobile/image-graphic-design.jpg)",
+        "orange-mobile": "url(/images/mobile/image-photography.jpg)",
+        "cherry-desktop": "url(/images/desktop/image-graphic-design.jpg)",
+        "orange-desktop": "url(/images/desktop/image-photography.jpg)",
+      }
+    },
+  },
+  plugins: [],
+}
